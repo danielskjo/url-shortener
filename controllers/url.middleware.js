a457a13469de146a74a9a70a14dff5dbbeef87f2
Daniel Jo <danieljo@DJs-MacBook-Pro.local>
2021-07-12 21:45:04 -0700

    Added try catch

--- controllers/url.middleware.js
+++ controllers/url.middleware.js
@@ -6,54 +6,74 @@ import shortid from "shortid";
 import Url from "../models/url.model.js";
 
 const getUrls = asyncHandler(async (req, res) => {
-  const urls = await Url.find();
-  res.json(urls);
+  try {
+    const urls = await Url.find();
+    res.json(urls);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 const shortenUrl = asyncHandler(async (req, res) => {
-  const { originalUrl } = req.body;
-  const domain = config.get("domain");
+  try {
+    const { originalUrl } = req.body;
+    const domain = config.get("domain");
 
-  const urlPath = shortid.generate();
+    const urlPath = shortid.generate();
 
-  let url = await Url.findOne({ originalUrl });
+    let url = await Url.findOne({ originalUrl });
 
-  if (url) {
-    res.json(url);
-  } else {
-    const newUrl = domain + "/api/urls/" + urlPath;
+    if (url) {
+      res.json(url);
+    } else {
+      const newUrl = domain + "/api/urls/" + urlPath;
 
-    const url = new Url({
-      urlPath,
-      originalUrl,
-      newUrl,
-    });
+      const url = new Url({
+        urlPath,
+        originalUrl,
+        newUrl,
+      });
 
-    const createdUrl = await url.save();
+      const createdUrl = await url.save();
 
-    res.status(201).json(createdUrl);
+      res.status(201).json(createdUrl);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
 const getUrl = asyncHandler(async (req, res) => {
-  const url = await Url.findOne({ urlPath: req.params.path });
+  try {
+    const url = await Url.findOne({ urlPath: req.params.path });
 
-  if (url) {
-    res.redirect(url.originalUrl);
-    res.json(url);
-  } else {
-    res.status(404).json({ msg: "URL not found" });
+    if (url) {
+      res.redirect(url.originalUrl);
+      res.json(url);
+    } else {
+      res.status(404).json({ message: "URL not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
 const deleteUrl = asyncHandler(async (req, res) => {
-  const url = await Url.findOne({ urlPath: req.params.path });
+  try {
+    const url = await Url.findOne({ urlPath: req.params.path });
 
-  if (url) {
-    await url.remove();
-    res.json({ msg: "Url removed" });
-  } else {
-    res.status(404).json({ msg: "URL not found" });
+    if (url) {
+      await url.remove();
+      res.json({ message: "Url removed" });
+    } else {
+      res.status(404).json({ message: "URL not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
